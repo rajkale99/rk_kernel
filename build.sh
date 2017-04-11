eb98bbdf1646f21f02d66e15932aa3ddb348a80a
Francisco Franco <franciscofranco.1990@gmail.com>
2017-04-11 17:38:41 +0000

    drivers: fingerprint: boost cpufreq on unlock
    To reduce wake-up latency when using the fingerprint scanner we
    register an input device and send a "key press" when the scanner
    is being used during screen off. This will boost the cpus through
    cpu-boost and reduce the screen on latency.
    
    General idea "stole" from this commit sultanxda/android_kernel_oneplus_msm8996@f194585
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- build.sh
+++ build.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-echo Welcome To Automated Kernel Compiler For smartron
-echo By rajkale99
 
-echo Cleaning some old files
-rm -rf $(pwd)output
-rm -rf $(pwd)dsm/zImage
-make clean
-make mrproper
-echo Cleaned Some Old Crappy File
 
-export CROSS_COMPILE=/home/rajkale99/google4.9/bin/aarch64-linux-android-
+#Colors
+black='\033[0;30m'
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+purple='\033[1;35m'
+cyan='\033[0;36m'
+nc='\033[0m'
 
-echo Compiler Set Succesfully
+echo -e "$cyan---------------- ----";
+echo -e "------------------------\n";
+echo -e "██████╗--█████╗-- ███████╗";
+echo -e "██╔══██╗██╔══██╗     ██  ╝";
+echo -e "██████╔╝███████║     ██ ║";
+echo -e "██╔══██╗██╔══██║     ██ ║";
+echo -e "██║--██║██║--██║ ██████ ║";
+echo -e "╚═╝--╚═╝╚═╝--╚═╝ ╚══════╝\n";
 
-mkdir $(pwd)/output
 
-echo Setting Compiler To Build For ARM64
-
+echo -e "$cyan  Welcome To Automated Kernel Compiler For sander"
+echo -e "$red  By rajkale99"
+make clean && make mrproper && rm -rf output && rm -rf ~/ank/zImage && rm -rf ~/ank/Image.gz-dtb
+echo -e "$purple Cleaned Some Old Crappy Files"
+export CROSS_COMPILE=~/toolchain/bin/aarch64-linux-android-
+#mkdir output && mkdir modules && mkdir -p output/modules
+echo -e "$green Compiler Set Sucessful"
 export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER="Rajkale99"
-export KBUILD_BUILD_HOST="rk_kernel"
-
-echo Picking rk_kernel configs
-
-make -j4 -C $(pwd) O=output rk_defconfig
-
-echo Compiling awesomeness
-
+echo -e "$brown  Compiler Set To Build For ARM64"
+make -C $(pwd) O=output rk_defconfig
+echo -e "$blue  Picked Stock Config"
 make -j32 -C $(pwd) O=output
-echo Building Has Been Completed
+cp -rf ~/msm8976/output/arch/arm64/boot/Image.gz-dtb ~/ank/
+cd ~/ank/
+mv Image.gz-dtb zImage
+#c#d ~/r/output && mkdir -p modules/
+#find ~/rkoreo/output -name '*.ko' -exec cp {} modules/ \;
+#cd ~/rkoreo/output/modules
+#~/toolchains//aarch64-linux-android-8.x/bin/aarch64-linux-android-strip --strip-unneeded *.ko && mkdir pronto && cp -a wlan.ko pronto_wlan.ko && mv pronto_wlan.ko pronto
+#mv ~/rkoreo/output/modules/ ~/rkoreo/dsm
+echo -e "$nc  Building Has Been Completed"
+cd ~/msm8976
