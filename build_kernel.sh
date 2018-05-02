60052ee576cdc69400e02ac7f2a45d0ac24c4586
Rajkale99 <kaleraj.rk@gmail.com>
2018-05-02 14:09:15 +0000

    strip all modules

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Welcome To Automated Kernel Compiler For smartron
 echo By rajkale99
-make clean && make mrproper && rm -rf output
+make clean && make mrproper && rm -rf output && rm -rf dsm/modules
 echo Cleaned Some Old Crappy Files
 export CROSS_COMPILE=~/toolchains/linaro/bin/aarch64-linux-android-
 mkdir output && mkdir mod && mkdir -p output/mod
@@ -13,9 +13,10 @@ make -C $(pwd) O=output menuconfig
 echo Picked Stock Config
 make -j16 -C $(pwd) O=output
 cp ~/rkkernel/output/arch/arm64/boot/Image.gz-dtb ~/rkkernel/dsm
-mv ~/rkernel/dsm/Image.gz-dtb zImage
+mv ~/rkkernel/dsm/Image.gz-dtb ~/rkkernel/dsm/zImage
+cd ~/rkkernel/output && mkdir -p mod/
 find ~/rkkernel/output -name '*.ko' -exec cp {} mod/ \;
-cd mod
-STRIP=~/toolchains/linaro/bin/aarch64-linux-android-strip --strip-unneeded *.ko && mkdir pronto && cp -a wlan.ko pronto_wlan.ko && mv pronto_wlan.ko pronto
-
+cd ~/rkkernel/output/mod
+~/toolchains/linaro/bin/aarch64-linux-android-strip --strip-unneeded *.ko && mkdir pronto && cp -a wlan.ko pronto_wlan.ko && mv pronto_wlan.ko pronto
+mv ~/rkkernel/output/mod/ ~/rkkernel/dsm
 echo Building Has Been Completed
