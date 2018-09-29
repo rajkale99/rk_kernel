74f94901a397cec10921839121ceb4b56ebbb470
rajkale99 <kaleraj.rk@gmail.com>
2018-09-30 02:55:41 +0530

    build with gcc 8.1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo Welcome To Automated Kernel Compiler For smartron
 echo By rajkale99
 make clean && make mrproper && rm -rf output && rm -rf dsm/modules && rm -rf dsm/zImage
 echo Cleaned Some Old Crappy Files
-export CROSS_COMPILE=~/toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/toolchain/bin/aarch64-opt-linux-android-
 
 
 mkdir output && mkdir modules && mkdir -p output/modules
@@ -13,11 +13,11 @@ echo Compiler Set To Build For ARM64
 make -C $(pwd) O=output rk_defconfig
 echo Picked Stock Config
 make -j16 -C $(pwd) O=output
-cp ~/rk_kernel/output/arch/arm64/boot/Image.gz-dtb ~/rk_kernel/dsm
-mv ~/rk_kernel/dsm/Image.gz-dtb ~/rk_kernel/dsm/zImage
-cd ~/rk_kernel/output && mkdir -p modules/
-find ~/rk_kernel/output -name '*.ko' -exec cp {} modules/ \;
-cd ~/rk_kernel/output/modules
-~/toolchain/bin/aarch64-linux-android-strip --strip-unneeded *.ko && mkdir pronto && cp -a wlan.ko pronto_wlan.ko && mv pronto_wlan.ko pronto
-mv ~/rk_kernel/output/modules/ ~/rk_kernel/dsm
+cp ~/rkkernel/srt/output/arch/arm64/boot/Image.gz-dtb ~/rkkernel/srt/dsm
+mv ~/rkkernel/srt/dsm/Image.gz-dtb ~/rkkernel/srt/dsm/zImage
+cd ~/rkkernel/srt/output && mkdir -p modules/
+find ~/rkkernel/srt/output -name '*.ko' -exec cp {} modules/ \;
+cd ~/rkkernel/srt/output/modules
+~/toolchain/bin/aarch64-opt-linux-android-strip --strip-unneeded *.ko && mkdir pronto && cp -a wlan.ko pronto_wlan.ko && mv pronto_wlan.ko pronto
+mv ~/rkkernel/srt/output/modules/ ~/rkkernel/srt/dsm
 echo Building Has Been Completed
