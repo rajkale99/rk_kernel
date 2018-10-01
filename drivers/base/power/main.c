b8d75dc56e410ac738be9d92b6d4fdccd9a4fad8
rajkale99 <kaleraj.rk@gmail.com>
2018-10-01 23:34:40 +0530

    boeffla_wl_blocker: export get_active_wakeup_sources

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -585,6 +585,8 @@ static int device_resume_early(struct device *dev, pm_message_t state)
 	return error;
 }
 
+extern void print_active_wakeup_sources(void);
+
 /**
  * dpm_resume_early - Execute "early resume" callbacks for all devices.
  * @state: PM transition of the system being carried out.
@@ -594,10 +596,9 @@ static void dpm_resume_early(pm_message_t state)
 	ktime_t starttime = ktime_get();
 
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
-	pm_print_active_wakeup_sources();
+	print_active_wakeup_sources();
 #endif
 
-	trace_suspend_resume(TPS("dpm_resume_early"), state.event, true);
 	mutex_lock(&dpm_list_mtx);
 	while (!list_empty(&dpm_late_early_list)) {
 		struct device *dev = to_device(dpm_late_early_list.next);
